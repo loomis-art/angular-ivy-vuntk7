bc79220b828cf9ec6713fe07c4679cff209c2164
loomis-art <ozan.serdar@tr.loomis.com>
2020-05-17 21:08:04 +0300

    1 file modified

--- src/app/hive/types/ResultViewModel.ts
+++ src/app/hive/types/ResultViewModel.ts
@@ -1,3 +1,6 @@
+/*
+*
+*/
 export class ResultViewModel {
   type: string;
   HasError: boolean;
@@ -10,4 +13,23 @@ export class ResultViewModel {
 interface IException {
   Message: string;
   InnerException?: IException;
+}
+
+/*
+*
+*/
+export class ResultViewModelGeneric<ResultType> {
+    type: string;
+    HasError: boolean;
+    Hata: string;
+    toast: 'none' | 'success' | 'info' | 'warning' | 'error';
+    Exception: IException;
+    MesajBaslik: string;
+    Mesaj: string;
+    data: ResultType;
+}
+
+interface IException {
+    Message: string;
+    InnerException?: IException;
 }
