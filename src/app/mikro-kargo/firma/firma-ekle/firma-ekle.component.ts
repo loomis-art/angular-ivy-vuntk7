9258d3e7294938ecce66e4ff8ab8e3596431a796
loomis-art <ozan.serdar@tr.loomis.com>
2020-05-17 21:01:37 +0300

    4 files modified, 1 added

--- src/app/mikro-kargo/firma/firma-ekle/firma-ekle.component.ts
+++ src/app/mikro-kargo/firma/firma-ekle/firma-ekle.component.ts
@@ -6,7 +6,51 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./firma-ekle.component.css"]
 })
 export class FirmaEkleComponent implements OnInit {
-  firma: IMikroKargoFirma = { TicariUnvan: "adsasdad" };
+   firma: ProfileFirma = {
+        Il: undefined,
+        Ilce: undefined,
+        MuhasebeMail: undefined,
+        Telefon: undefined,
+        VergiNumarasi: undefined,
+        Yetkili: undefined,
+        Adi: '',
+        Adres: '',
+        Adresler: [],
+        Aktif: false,
+        Aldiklari: [],
+        Alici: false,
+        AltFirmalar: [],
+        ApplicationUserFirmas: [],
+        BolgeAdi: '',
+        BolgeIdd: 0,
+        Faturalari: [],
+        FirmaKodu: '',
+        FirmaTuru: undefined,
+        Gonderdikleri: [],
+        Gonderici: false,
+        Id: '',
+        IletisimBilgileri: [],
+        KargoKodu: '',
+        Kargolari: [],
+        KisaAdi: '',
+        MKodu: '',
+        MuhasebeKodu: '',
+        MusteriTuru: '',
+        SigortaFiyati: undefined,
+        SozlesmeBaslangici: undefined,
+        Sozlesmeleri: [],
+        SozlesmeyiYapan: '',
+        SubeAdi: '',
+        SubeIdd: 0,
+        SubeliFirma: false,
+        Type: 0,
+        Unvani: '',
+        UpsKargo: undefined,
+        VergiDairesi: '',
+        VergiNo: '',
+        YurticiKargo: undefined
+    };
+
 
   constructor() {}
 
